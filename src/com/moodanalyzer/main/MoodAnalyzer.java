6d1f09d63285cf3b2364371f52905f1fa09e2980
manikiranp <manikiranp3@gmail.com>
2020-11-22 18:32:34 +0530

    moodanalyazer junit test cases passed for happy and sad

--- src/com/moodanalyzer/main/MoodAnalyzer.java
+++ src/com/moodanalyzer/main/MoodAnalyzer.java
@@ -1,18 +1,7 @@
 package com.moodanalyzer.main;
 
 public class MoodAnalyzer {
-	
-	private String message;
-	
-	public MoodAnalyzer(String message) {
-		super();
-		this.message = message;
-	}
 
-	
-	public MoodAnalyzer() {
-		
-	}
 
 	public String Analyzemood(String mood) {
 		if (mood.toLowerCase().contains("sad"))
